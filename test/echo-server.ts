88621dd307947aee8b65ede96864e0cce65f73e1
Corey Alix <coreyalix@hotmail.com>
2020-08-06 14:24:24 -0400

    echo server content-type is always "echo/text"

--- test/echo-server.ts
+++ test/echo-server.ts
@@ -4,26 +4,27 @@ import { verbose } from "../server/fun/stringify";
 export class EchoServer {
     private server: http.Server | null = null;
 
-    constructor(private options: {
-        port: number;
-    }) {
-    }
+    constructor(
+        private options: {
+            port: number;
+        }
+    ) {}
 
     start() {
         if (!this.server) {
             this.server = http.createServer(async (req, res) => {
+                res.setHeader("content-type", "echo/text");
+
                 let ended = false;
                 res.write("echo(");
-                req
-                    .on("end", () => {
-                        ended = true;
-                        res.write(")");
-                        res.end();
-                    })
-                    .on("data", data => {
-                        if (ended) throw "CANNOT WRITE ANY MORE DATA";
-                        res.write(data);
-                    });
+                req.on("end", () => {
+                    ended = true;
+                    res.write(")");
+                    res.end();
+                }).on("data", (data) => {
+                    if (ended) throw "CANNOT WRITE ANY MORE DATA";
+                    res.write(data);
+                });
             });
             this.server.listen(this.options.port);
             verbose(`echo server listening at port ${this.options.port}`);
@@ -31,11 +32,9 @@ export class EchoServer {
     }
 
     stop() {
-        if (!this.server)
-            return;
+        if (!this.server) return;
         this.server.close();
         this.server = null;
         verbose("echo off");
     }
-
 }
