158cc7bc53e93782ed036dc796955b697403067c
COREY ALIX <corey.alix@infor.com>
2021-09-29 21:38:14 -0400

    Remove all transpiled resources

--- bin/server/fun/bufferToString.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bufferToString = void 0;
-function bufferToString(buffer) {
-    return buffer.map(v => String.fromCharCode(v)).join("");
-}
-exports.bufferToString = bufferToString;
-//# sourceMappingURL=bufferToString.js.map
--- bin/server/fun/isBinaryMimeType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isBinaryMimeType = void 0;
-function isBinaryMimeType(mimeType) {
-    return ["image/", "binary/"].some((v) => mimeType.startsWith(v));
-}
-exports.isBinaryMimeType = isBinaryMimeType;
-//# sourceMappingURL=isBinaryMimeType.js.map
