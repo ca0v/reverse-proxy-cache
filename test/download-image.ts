1837c3b3df0a18b4324bb6549033563e4b596b35
COREY ALIX <corey.alix@infor.com>
2021-11-20 03:19:35 -0500

    Adds test which caused server to crash

--- test/download-image.ts
+++ test/download-image.ts
@@ -1,5 +1,5 @@
 import { HttpsGet } from "../server/fun/http-get.js";
-import { ok, equal } from "assert";
+import { ok, equal, fail } from "assert";
 import { run, Server as ProxyServer } from "../server.js";
 import { IConfig } from "#@app/server/contracts.js";
 
@@ -22,6 +22,18 @@ const config: IConfig = {
   },
 };
 
+describe("Tests invalid url", () => {
+  it("Tests invalid url", async () => {
+    let cacheUrl = `http://localhost:${proxyPort}/mock/test/MapIcons/Invalid.png`;
+    try {
+      const response = await got.get(cacheUrl);
+      fail("expected failure");
+    } catch (ex) {
+      ok((ex + "").includes("ECONNREFUSED"));
+    }
+  });
+});
+
 describe("download-image tests", () => {
   let proxy: ProxyServer | void;
 
@@ -40,12 +52,10 @@ describe("download-image tests", () => {
     let cacheUrl = `http://localhost:${proxyPort}/mock/test/MapIcons/AbandonedVehicle.png`;
     let response1 = await got.get(cacheUrl);
     ok(
-      [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a].every(
-        (v, i) => {
-          equal(v, response1.body[i], `image header ${i}`);
-          return v === response1.body[i];
-        }
-      ),
+      [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a].every((v, i) => {
+        equal(v, response1.body[i], `image header ${i}`);
+        return v === response1.body[i];
+      }),
       "PNG header is correct"
     );
 
