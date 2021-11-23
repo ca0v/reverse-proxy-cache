0f51daaf950f0ff0ae0a4bd3dc8c23875594bdfc
COREY ALIX <corey.alix@infor.com>
2021-11-23 13:07:53 -0500

    fix: shutdown plugin was not testing for system?shutdown query string

--- server/plugins/ShutdownSystemPlugin.ts
+++ server/plugins/ShutdownSystemPlugin.ts
@@ -1,21 +1,31 @@
+import * as url from "url";
 import { dumpHeaders } from "#@app/test/dumpHeaders.js";
 import type { IncomingMessage, ServerResponse } from "http";
 import type { Server } from "../../server.js";
-import { verbose } from "../fun/stringify.js";
 import { setHeaders } from "../setHeaders.js";
+import { stringify, verbose as dump, verbose } from "../fun/stringify.js";
 
 export class ShutdownSystemPlugin {
-  constructor(private server: Server) { }
+  constructor(private server: Server) {}
 
   process(req: IncomingMessage, res: ServerResponse) {
     if (req.method !== "GET") return false;
+    const query = url.parse(req.url || "", true).query as {
+      shutdown: "shutdown";
+    };
+    if (typeof query.shutdown === "undefined") {
+      dump("shutdown expects a 'shutdown' query", req.url, query);
+      return false;
+    }
     verbose("ShutdownSystemPlugin");
     setHeaders(res, {
       "Content-Type": "application/text",
       "Access-Control-Allow-Credentials": "true",
       "Access-Control-Allow-Methods": "GET",
       "Access-Control-Allow-Headers": "",
-      "Access-Control-Allow-Origin": <string>(req.headers.origin || req.headers.referer || "localhost"),
+      "Access-Control-Allow-Origin": <string>(
+        (req.headers.origin || req.headers.referer || "localhost")
+      ),
     });
     dumpHeaders(res.getHeaders());
     res.write(`shutdown`);
--- test/server/shutdown-command.ts
+++ test/server/shutdown-command.ts
@@ -3,33 +3,64 @@ import { HttpsGet } from "../../server/fun/http-get.js";
 import * as assert from "assert";
 
 describe("tests the /system/shutdown command", () => {
-    it("starts and stops the proxy", async () => {
-        const proxy = new Server({
-            "reverse-proxy-cache": {
-                port: "3002",
-                verbose: true,
-                "reverse-proxy-db": "ca0v.sqlite",
-                "proxy-pass": []
-            }
-        });
-        await proxy.start();
-        const got = new HttpsGet();
-        let response = await got.get("http://localhost:3002/system?delete=500");
-        response = await got.get("http://localhost:3002/system?shutdown");
-        assert.equal(response.statusCode, 200, "system/shutdown 200");
-        const data = response.body;
-        assert.equal(data, "shutdown");
-        try {
-            await got.get("http://localhost:3002/system?delete=500");
-            throw "expected ECONNREFUSED exception";
-        } catch (ex) {
-            assert.ok((ex + "").startsWith("Error: connect ECONNREFUSED"), ex + "");
-        }
-        try {
-            proxy.stop(); // already stopped
-            throw "should already be stopped";
-        } catch (ex) {
-            assert.ok((ex + "").startsWith("Error: SQLITE_MISUSE: Database is closed"), ex + "");
-        }
+  it("ensures invalid system request does not stop the server", async () => {
+    const proxy = new Server({
+      "reverse-proxy-cache": {
+        port: "3002",
+        verbose: true,
+        "reverse-proxy-db": "ca0v.sqlite",
+        "proxy-pass": [],
+      },
     });
-})
+    await proxy.start();
+    const got = new HttpsGet();
+    let response = await got.get("http://localhost:3002/system?delete=500");
+    response = await got.get("http://localhost:3002/system?shut");
+    assert.equal(response.statusCode, 500, "statusCode");
+    assert.equal(
+      response.body,
+      "no configuration found for this endpoint",
+      "body"
+    );
+    assert.equal(
+      response.statusMessage,
+      "Internal Server Error",
+      "statusMessage: unable to process request"
+    );
+    await got.get("http://localhost:3002/system?delete=500");
+    proxy.stop(); // already stopped
+  });
+
+  it("starts and stops the proxy", async () => {
+    const proxy = new Server({
+      "reverse-proxy-cache": {
+        port: "3002",
+        verbose: true,
+        "reverse-proxy-db": "ca0v.sqlite",
+        "proxy-pass": [],
+      },
+    });
+    await proxy.start();
+    const got = new HttpsGet();
+    let response = await got.get("http://localhost:3002/system?delete=500");
+    response = await got.get("http://localhost:3002/system?shutdown");
+    assert.equal(response.statusCode, 200, "system/shutdown 200");
+    const data = response.body;
+    assert.equal(data, "shutdown");
+    try {
+      await got.get("http://localhost:3002/system?delete=500");
+      throw "expected ECONNREFUSED exception";
+    } catch (ex) {
+      assert.ok((ex + "").startsWith("Error: connect ECONNREFUSED"), ex + "");
+    }
+    try {
+      proxy.stop(); // already stopped
+      throw "should already be stopped";
+    } catch (ex) {
+      assert.ok(
+        (ex + "").startsWith("Error: SQLITE_MISUSE: Database is closed"),
+        ex + ""
+      );
+    }
+  });
+});
